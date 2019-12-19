eedeb8243b2868d2a2e60c7f4752f5c30ecf8189
jaxin007 <swatdimonbil@gmail.com>
2019-12-19 14:09:03 +0200

    added java tasks

---
+++ Java_OOP/lab1.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class lab1 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("First");
+        frame.setSize(100, 80);
+        frame.setVisible(true);
+
+        JButton btn = new JButton("Start");
+        frame.getContentPane().add(btn, BorderLayout.EAST);
+
+        JTextField tf = new JTextField();
+        frame.getContentPane().add(tf, BorderLayout.CENTER);
+
+        JTextField tf1 = new JTextField();
+        frame.getContentPane().add(tf1, BorderLayout.SOUTH);
+
+        btn.setLocation(10, 10);
+        btn.setSize(160, 50);
+
+        btn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String s = tf.getText();
+                String result = fapfap(s);
+                tf1.setText(result);
+            }
+        });
+
+    }
+
+    static String fapfap(String s) {
+        char[] c = s.toCharArray();
+        for (int i = 0; i < c.length - 1; i += 2) {
+            char t = c[i];
+            c[i] = c[i + 1];
+            c[i + 1] = t;
+        }
+        return String.valueOf(c);
+    }
+}
---
+++ Java_OOP/lab4.java
@@ -0,0 +1,94 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class Window {
+    boolean open;// провірити вікно
+    float length = 8;
+    float height = 7;
+    int glassCount;
+    String material;
+    String color;
+
+    Window() {
+        open = false;
+        glassCount = 3;
+        color = "White";
+        material = "tree";
+
+    }
+
+    Window(float l, float h) {
+        this();
+        length = l;
+        height = h;
+        System.out.println("Lenqth:" + length + "Height:" + height);
+    }
+
+    Window(String m) {
+        System.out.println("Material:" + m);
+        material = m;
+    }
+
+    void printWindow() { // метод
+
+        System.out.println("Open window: " + open + "; Length:" + length + "; Height:" + height + "; Material:"
+                + material + "; Color:" + color);
+    }
+
+    void close() {
+        open = false;// об'єкт
+        System.out.println("window close");
+    }
+
+    void open() {
+        open = true;// об'єкт
+        System.out.println("window open");
+    }
+
+    float getSquare() {
+        return length * height;
+    }
+}
+
+class lab4 {
+    public static void main(String[] args) {
+        JButton btn = new JButton("Create");
+        JFrame frame = new JFrame("First");
+        JButton cl = new JButton("Close");
+        JButton op = new JButton("Open");
+        JButton square = new JButton("Square");
+        JTextField square1 = new JTextField();
+        Window w = new Window(8, 6);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(cl, BorderLayout.EAST);
+        frame.getContentPane().add(btn, BorderLayout.WEST);
+        frame.getContentPane().add(square, BorderLayout.CENTER);
+        frame.getContentPane().add(op, BorderLayout.SOUTH);
+        frame.setSize(250, 200);
+        frame.setVisible(true);
+        btn.setLocation(10, 10);
+        btn.setSize(160, 50);
+
+        cl.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                w.close();
+                w.printWindow();
+            }
+        });
+
+        op.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                w.open();
+                w.printWindow();
+            }
+        });
+
+        square.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Square=" + w.getSquare());
+            }
+        });
+
+    }
+}
